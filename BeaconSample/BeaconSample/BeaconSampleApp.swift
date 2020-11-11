e45bf5b96537eeee1e65bf39aa262f5548c6552b
ohayota <atoy.ryujin.93621339@gmail.com>
2020-11-11 21:47:42 +0900

    プロジェクト追加

---
+++ BeaconSample/BeaconSample/BeaconSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconSampleApp.swift
+//  BeaconSample
+//
+//  Created by Yota Nakamura on 2020/11/11.
+//
+
+import SwiftUI
+
+@main
+struct BeaconSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
