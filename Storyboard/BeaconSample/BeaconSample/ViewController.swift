87f9d8380dbc8503e2fc0ecae72b7c706b2b993e
ohayota <atoy.ryujin.93621339@gmail.com>
2020-11-12 21:36:15 +0900

    Storyboard版の新規作成

---
+++ Storyboard/BeaconSample/BeaconSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeaconSample
+//
+//  Created by Yota Nakamura on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
